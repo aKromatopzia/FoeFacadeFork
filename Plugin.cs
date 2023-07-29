221acccceba9ec52b4b8385951fe94e2e8747ab1
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2023-07-29 01:39:17 -0700

    Added ManualLogSource Flipped SaveData portrait if enemy skin Flipped Initiative portrait if enemy skin Renamed FlipEnemySkin to FlipEnemySkinInCombat to fit naming convention Prevented Imp Altar from loading due to issues with the animated preview Added redundancy check to prevent duplicate skindata from getting added SkinData addition is now based on the npc's animated game object rather than their name Switched their small portrait to using their SpriteSpeed instead of their SpritePortrait to fit in line with the game

--- Plugin.cs
+++ Plugin.cs
@@ -17,14 +17,79 @@ namespace AtOEnemySkinsForPlayerMod
     public class Plugin : BaseUnityPlugin
     {
         public static List<string> skinsToFlip = new List<string>();
+        public static BepInEx.Logging.ManualLogSource _logger;
 
         public void Awake()
         {
+            _logger = Logger;
             AtOSkinExtender.Plugin.onCreateSkins += CreateEnemySkinsForHeroes;
 
-            On.HeroItem.Start += FlipEnemySkin;
+            //Which is better? Making 100+ copies of the sprite but flipped
+            //or just flipping each one wherever they show up?
+            On.HeroItem.Start += FlipEnemySkinInCombat;
+            //help
+            //On.CharPopup.SetHeroAnimated += FlipEnemySkinInPreview;
+            On.InitiativePortrait.Init += FlipInitiativePortrait;
+            //should this be an IL hook????
+            On.MenuSaveButton.SetGameData += FlipSaveDataPortrait;
         }
-        public static void FlipEnemySkin(On.HeroItem.orig_Start orig, HeroItem self)
+
+        private void FlipSaveDataPortrait(On.MenuSaveButton.orig_SetGameData orig, MenuSaveButton self, GameData _gameData)
+        {
+            orig(self, _gameData);
+            Hero[] array2 = JsonHelper.FromJson<Hero>(_gameData.TeamAtO);
+            for (int l = 0; l < array2.Length; l++)
+            {
+                if (array2[l].SubclassName != null && Globals.Instance.GetSubClassData(array2[l].SubclassName) != null)
+                {
+                    SkinData skinData;
+                    if (array2[l].SkinUsed == null || array2[l].SkinUsed == "")
+                    {
+                        skinData = Globals.Instance.GetSkinData(Globals.Instance.GetSkinBaseIdBySubclass(array2[l].SubclassName));
+                    }
+                    else
+                    {
+                        skinData = Globals.Instance.GetSkinData(array2[l].SkinUsed);
+                    }
+                    var localScale = self.imgHero[l].transform.localScale;
+                    if (skinData != null && skinsToFlip.Contains(skinData.SkinId))
+                    {
+                        self.imgHero[l].transform.localScale = new Vector3(-Mathf.Abs(localScale.x), localScale.y, localScale.z);
+                    } else
+                    {
+                        self.imgHero[l].transform.localScale = new Vector3(Mathf.Abs(localScale.x), localScale.y, localScale.z);
+                    }
+                }
+            }
+        }
+
+        private void FlipInitiativePortrait(On.InitiativePortrait.orig_Init orig, InitiativePortrait self, int _position)
+        {
+            orig(self, _position);
+            if (self.charSprite && self.Hero != null && skinsToFlip.Contains(self.Hero.skinUsed))
+            {
+                var localScale = self.charSprite.transform.localScale;
+                self.charSprite.transform.localScale = new Vector3(-Mathf.Abs(localScale.x), localScale.y, localScale.z);
+            }
+        }
+
+        private void FlipEnemySkinInPreview(On.CharPopup.orig_SetHeroAnimated orig, CharPopup self, GameObject heroSkin)
+        {
+            orig(self, heroSkin);
+            if (self.heroAnimated)
+            {
+                string activeSkin = PlayerManager.Instance.GetActiveSkin(self.SCD.Id);
+                var localScale = self.heroAnimated.transform.localScale;
+                var newX = Mathf.Abs(localScale.x);
+                if (skinsToFlip.Contains(activeSkin))
+                {
+                    newX *= -1;
+                }
+                self.heroAnimated.transform.localScale = new Vector3(newX, localScale.y, localScale.z);
+            }
+        }
+
+        public static void FlipEnemySkinInCombat(On.HeroItem.orig_Start orig, HeroItem self)
         {
             orig(self);
             if (self.CharImageT && self.Hero != null && skinsToFlip.Contains(self.Hero.skinUsed))
@@ -36,17 +101,38 @@ namespace AtOEnemySkinsForPlayerMod
 
         public static void CreateEnemySkinsForHeroes()
         {
+            _logger.LogMessage("Creating enemy skins!");
+
+            //There's a bunch of alternate ones that I think(?) are identical with some purpose
+            //like {ariel} and {ariel_b}
+            //fackin annoying!!!!
+            List<GameObject> usedNPCDisplays = new List<GameObject>();
+
             foreach (var npcSource in Globals.Instance._NPCsSource)
             {
                 var npc = npcSource.Value;
+                //loading the altar gameobject breaks the preview because its a single thing
+                //and its expected an animated something idk
+                if (npc.NPCName == "Imp Altar") continue;
+
+                if (usedNPCDisplays.Contains(npc.GameObjectAnimated))
+                    continue;
+                usedNPCDisplays.Add(npc.GameObjectAnimated);
+                //Debug.Log($"====NPC {npcSource.Key}");
                 foreach (var subclass in Globals.Instance._SubClassSource)
                 {
+                    //they're not normally selectable.
+                    //if they're able to then ill remove this clause
+                    if (subclass.Key.StartsWith("young"))
+                        continue;
+                    //Debug.Log($"====NPC {npc.NPCName} for {subclass.Key}");
                     var skinData = AtOSkinExtender.Assets.CreateSkinData(subclass.Key,
                         npc.NPCName,
+                        $"{subclass.Key}Skin{npc.GameObjectAnimated.name}",
                         0,
+                        npc.SpriteSpeed,
                         npc.SpritePortrait,
-                        npc.SpritePortrait,
-                        npc.SpritePortrait,
+                        npc.SpriteSpeed,
                         npc.SpritePortrait,
                         npc.GameObjectAnimated);
                     if (skinData == null)
